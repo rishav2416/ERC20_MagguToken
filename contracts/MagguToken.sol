ca1e7598a750ae28192de68d4fb5b820b45890a9
Rishav <rishavranjan204@gmail.com>
2021-02-13 22:21:17 +0530

    Transfer Function

--- contracts/MagguToken.sol
+++ contracts/MagguToken.sol
@@ -6,10 +6,20 @@ contract MagguToken{
     string public name="MagguToken";
     //Symbol
     string public symbol ="MUG";
+    //Standard
+    string public standard= "MagguToken v1.0";
+
     //Total Supply
     uint256 public totalSupply;
     
     mapping(address=> uint256) public balanceOf;
+
+    //Transfer Event
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value   
+    );
 
     //Constructor
     constructor(uint256 _initialSupply){
@@ -18,7 +28,21 @@ contract MagguToken{
         totalSupply = _initialSupply;
     }
 
+    //Transfer function
+    function transfer(address _to, uint256 _value) public returns (bool){
+            //Exception if account doesn't have enough
+            require(balance[msg.sender]>=_value); //if it is true, continue function execution
+            
+            //Transfer balance
+            balanceOf[msg.sender] -=_value;
+            balanceOf[_to] +=_value;
+
+            //Transfer event
+            Transfer(msg.sender, _to, _value);
+
+            return true;
 
 
+    }
 
 }
--- test/MagguToken.js
+++ test/MagguToken.js
@@ -13,7 +13,11 @@ contract("MagguToken", function(accounts){
             return tokenInstance.symbol();
         }).then(function(symbol){
             assert.equal(symbol, "MUG", "Symbol matches");
+            return tokenInstance.standard();
+        }).then(function(standard){
+            assert.equal(standard, "MagguToken v1.0");
         });
+
     })
     it("sets the total supply upon deployment", function()
     {
@@ -27,5 +31,30 @@ contract("MagguToken", function(accounts){
             assert.equal(adminBalance.toNumber(), 1000000, "give the whole supply to admin");
         });
     });
+    it("Transfers ownership", function(){
+        return MagguToken.deployed().then(function(i){
+            tokenInstance=i;
+            assert(tokenInstance.transfer.call(accounts[1], 9999999999));
+        }).then(assert.fail).catch(function(e){
+            assert(error.message.indexOf('revert')>=0, "error msg must have a revert");
+            return tokenInstance.transfer.call(accounts[1], 250000, {from : accounts[0]});
+        }).then(function(success){
+            assert.equal(success, true, 'it returns true');
+            return tokenInstance.transfer(accounts[1], 250000, {from: accounts[0]});
+        }).then(function(receipt){
+            assert.equal(receipt.logs.length, 1, 'trigger one event');
+            assert.equal(receipt.logs[0].event, 'Transfer', 'should be the Trigger event');
+            assert.equal(receipt.logs[0].args._from, accounts[0], 'logs the account tokens are transferred from');
+            assert.equal(receipt.logs[0].args._to, accounts[1], 'logs the account token are transferred to');
+            assert.equal(receipt.logs[0].args._value, 250000, 'logs the transfer amount');
+            return tokenInstance.balanceOf(accounts[1]);
+
+        }).then(function(balance){
+            assert.equal(balance.toNumber(), 250000, 'money recieved by recieving account');
+            return tokenInstance.balanceOf(accounts[0]);
+        }).then(function(balance){
+            assert.equal(balance.toNumber(), 750000,, "money sent by sender");
+        }).
+    })
 
 })
