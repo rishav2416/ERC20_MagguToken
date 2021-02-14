25dbaff7083a5bbe99e5198698cdf89f58d5026d
Rishav <rishavranjan204@gmail.com>
2021-02-14 16:23:05 +0530

    Buy token

--- contracts/TokenSale.sol
+++ contracts/TokenSale.sol
@@ -2,17 +2,46 @@
 pragma solidity ^0.7.4;
 import "./MagguToken.sol";
 
-contract TokenSale{
+contract TokenSale {
     address admin;
     MagguToken public tokenContract;
     uint256 public tokenPrice;
-    
-    constructor(MagguToken _tokenContract, uint256 _tokenPrice){
+    uint256 public tokenSold; 
+
+    event Sell(
+        address _buyer,
+        uint256 _amount
+    );
+
+    constructor(MagguToken _tokenContract, uint256 _tokenPrice) {
         //Assign admin
-        admin=msg.sender;
+        admin = msg.sender;
         //Token Contract
-        tokenContract=_tokenContract;
+        tokenContract = _tokenContract;
         //Token Price set
-        tokenPrice =  _tokenPrice;
+        tokenPrice = _tokenPrice;
     }
+
+    //Multiply
+    function multiply(uint256 x, uint y) internal pure returns(uint z){
+        require(y==0 || (z-x*y)/y==x);
+    }
+//pure means they re not doing any transcation
+
+
+    //Buy Token
+    function buyToken(uint256 _numberOfTokens) public payable {
+    //Require that value is equal to tokens
+    require(multiply(_numberOfTokens, tokenPrice)==msg.value);
+    //Require that contract has enough tokens
+    require(tokenContract.balanceOf(address(this)) >= _numberOfTokens);
+    // Require that transfer is successful
+    require(tokenContract.transfer( msg.sender, _numberOfTokens ));
+    //Trigger event 
+    emit Sell(msg.sender, _numberOfTokens); 
+
+    // Keep track of tokenSold
+    tokenSold += _numberOfTokens;
+    }
+
 }
--- test/TokenSale.js
+++ test/TokenSale.js
@@ -1,19 +1,93 @@
 var TokenSale = artifacts.require("./TokenSale.sol");
+var MagguToken = artifacts.require("./MagguToken.sol");
 
-contract("TokenSale", function(accounts){
-    var tokenSaleInstance;
-    var tokenPrice = 1000000000000000;//in wei
+contract("TokenSale", function (accounts) {
+  var tokenSaleInstance;
+  var tokenInstance;
+  var admin = accounts[0];
+  var tokenPrice = 1000000000000000;
+  var buyer = accounts[1]; //in wei
+  var tokensAvailable = 750000;
+  var numberOfTokens;
 
-
-    it("initializes the contract with the correct values", function(){
-        return TokenSale.deployed().then(function(i){
-            tokenSaleInstance = i;
-            return tokenSaleInstance.address;
-        }).then(function(address){
+  it("initializes the contract with the correct values", function () {
+    return TokenSale.deployed()
+      .then(function (i) {
+        tokenSaleInstance = i;
+        return tokenSaleInstance.address;
+      })
+      .then(function (address) {
         assert.notEqual(address, 0x0, "has contract address");
         return tokenSaleInstance.tokenPrice();
-    }).then(function(price){
-            assert.equal(price, tokenPrice, "token price is correct");
+      })
+      .then(function (price) {
+        assert.equal(price, tokenPrice, "token price is correct");
+      });
+  });
+  it("facilitates Token Buy", function () {
+    return MagguToken.deployed()
+      .then(function (i) {
+        //Grabbing token instance first
+        tokenInstance = i;
+        return TokenSale.deployed();
+      })
+      .then(function (i) {
+        //Grabbing sale instance
+        tokenSaleInstance = i;
+        //Provision 75% tokens to token sale contract
+        return tokenInstance.transfer(
+          tokenSaleInstance.address,
+          tokensAvailable,
+          { from: admin }
+        );
+      })
+      .then(function (receipt) {
+        numberOfTokens = 10;
+        return tokenSaleInstance.buyToken(numberOfTokens, {
+          from: buyer,
+          value: numberOfTokens * tokenPrice
+        });
+      })
+      .then(function (receipt) {
+        assert.equal(receipt.logs.length, 1, "trigger one event");
+        assert.equal(
+          receipt.logs[0].event,
+          "Sell",
+          "should be the Trigger event"
+        );
+        assert.equal(
+          receipt.logs[0].args._buyer,
+          buyer,
+          "logs the account which buys tokens"
+        );
+        assert.equal(
+          receipt.logs[0].args._amount,
+          numberOfTokens,
+          "logs the number of tokens purchased"
+        );
+        return tokenSaleInstance.tokensSold();
+      })
+      .then(function (amount) {
+        assert.equal(
+          amount.toNumber(),
+          numberOfTokens,
+          "increments the amount of tokens sold"
+        );
+        return tokenInstance.balanceOf(buyer);
+      })
+      .then(function (balance) {
+        assert.equal(balance.toNumber(), numberOfTokens);
+        return tokenInstance.balanceOf(tokenSaleInstance.address);
+      })
+      .then(function (balance) {
+        assert.equal(balance.toNumber(), tokensAvailable - numberOfTokens);
+     // Try to buy tokens different from the ether value
+     return tokenSaleInstance.buyToken(numberOfTokens, { from: buyer, value: 1 });
+    }).then(assert.fail).catch(function(error) {
+      assert(error.message.indexOf('revert') >= 0, 'msg.value must equal number of tokens in wei');
+      return tokenSaleInstance.buyToken(800000, { from: buyer, value: numberOfTokens * tokenPrice })
+    }).then(assert.fail).catch(function(error) {
+      assert(error.message.indexOf('revert') >= 0, 'cannot purchase more tokens than available');
     });
+  });
 });
-})
