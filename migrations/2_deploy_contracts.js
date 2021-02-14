731d08deaa0d8aaed23bf6472c84d76dcb1acf2d
Rishav <rishavranjan204@gmail.com>
2021-02-14 13:15:32 +0530

    TokenSale #2

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,10 @@
 var MagguToken = artifacts.require("./MagguToken.sol");
 var TokenSale = artifacts.require("./TokenSale.sol");
 
-module.exports = function (deployer) {
-    deployer.deploy(MagguToken, 1000000).then(function(){
-        deployer.deploy(TokenSale, MagguToken.address, 1000000000000000);
-    });
-    
-};
+module.exports = function(deployer) {
+  deployer.deploy(MagguToken, 1000000).then(function() {
+    // Token price is 0.001 Ether
+    var tokenPrice = 1000000000000000;
+    return deployer.deploy(TokenSale, MagguToken.address, tokenPrice);
+  });
+};;
--- test/TokenSale.js
+++ test/TokenSale.js
@@ -1,19 +1,19 @@
 var TokenSale = artifacts.require("./TokenSale.sol");
 
-contract('TokenSale', function(accounts){
+contract("TokenSale", function(accounts){
     var tokenSaleInstance;
     var tokenPrice = 1000000000000000;//in wei
 
 
-    it("initializes the contract with the correct values", function(i){
+    it("initializes the contract with the correct values", function(){
         return TokenSale.deployed().then(function(i){
             tokenSaleInstance = i;
             return tokenSaleInstance.address;
         }).then(function(address){
         assert.notEqual(address, 0x0, "has contract address");
-        return tokenInstance.tokenPrice();
+        return tokenSaleInstance.tokenPrice();
     }).then(function(price){
-            assert.equal(pirce, tokenPrice, "token price is correct");
+            assert.equal(price, tokenPrice, "token price is correct");
     });
 });
 })
